7dd5dde46b38a7f7951660c5d158b66bb5228ead
Paulo Barreto <paulovictorgtb@hotmail.com>
2018-09-17 17:46:13 -0300

    Cria scroll suave para os links da página

--- js/animation.js
+++ js/animation.js
@@ -8,3 +8,30 @@ saibaMais.addEventListener('mouseover', () => {
 saibaMais.addEventListener('mouseout', () => {
     setaHome.classList.remove('desce-sobe');
 });
+
+const navItems = document.querySelectorAll('.barra-navegacao .nav-links');
+const saibaMaisLink = document.querySelector('.saiba-mais-links');
+
+function scrollToIdOnClick() {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.currentTarget);
+    scrollToPosition(to);
+}
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('Href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+    window.scroll ({
+        top: to,
+        behavior: "smooth",
+    })
+}
+
+navItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+});
+
+saibaMaisLink.addEventListener('click', scrollToIdOnClick);
